13741e5425590ec737c181f501321bddafcbaea4
Stevenozzo <stevencarlino46@live.com>
2024-09-23 17:01:51 +0200

    esercizio pet

--- js/index.js
+++ js/index.js
@@ -18,3 +18,23 @@ for (let i = 0; i < nUser; i++) {
   const utente = new User(nome, cognome, eta, abitazione);
   console.log(utente);
 }
+
+class Pet {
+  constructor(petName, ownerName, species, breed) {
+    this.petName = petName;
+    this.ownerName = ownerName;
+    this.species = species;
+    this.breed = breed;
+  }
+
+  sharesOwner(otherPet) {
+    return this.ownerName === otherPet.ownerName;
+  }
+}
+
+const pet1 = new Pet("Fido", "Mario Rossi", "Cane", "Labrador");
+const pet2 = new Pet("Micio", "Mario Rossi", "Gatto", "Persiano");
+const pet3 = new Pet("Rex", "Luigi Bianchi", "Cane", "Beagle");
+
+console.log(pet1.sharesOwner(pet2));
+console.log(pet1.sharesOwner(pet3));
